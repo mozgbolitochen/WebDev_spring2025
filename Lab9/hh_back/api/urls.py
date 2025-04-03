2bd2ab8b6954177f85469a64d48d7aad1624efc0
mozgbolitochen <157012061+mozgbolitochen@users.noreply.github.com>
2025-04-03 07:15:05 +0500

    Lab9

---
+++ Lab9/hh_back/api/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import company_list, company_detail, company_vacancies, vacancy_list, vacancy_detail, top_ten_vacancies
+
+urlpatterns = [
+    path('companies/', company_list, name='company-list'),
+    path('companies/<int:id>/', company_detail, name='company-detail'),
+    path('companies/<int:id>/vacancies/', company_vacancies, name='company-vacancies'),
+    path('vacancies/', vacancy_list, name='vacancy-list'),
+    path('vacancies/<int:id>/', vacancy_detail, name='vacancy-detail'),
+    path('vacancies/top_ten/', top_ten_vacancies, name='top-ten-vacancies'),
+]
